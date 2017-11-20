207f7f99bc155fc8d26771a050803f63108ecc4e
Chris Jensen <zigs@zigs.dk>
2017-11-20 21:55:24 +0100

    finish subscription logic

--- source/public/js/GUI/KeyMemory.ts
+++ source/public/js/GUI/KeyMemory.ts
@@ -9,6 +9,15 @@ type keyboardEvent = React.KeyboardEvent<HTMLAnchorElement>;
 type keyboardEventCallback = (keyboardEvent)=>void;
 type keyboardEventCallbackTable = Hash<Array<keyboardEventCallback>>;
 
+interface Subscriptions
+{
+	keyDown: keyboardEventCallbackTable
+	keyUp: keyboardEventCallbackTable
+	keyDownAlone: keyboardEventCallbackTable
+	keyUpClean: keyboardEventCallbackTable
+}
+
+
 export class KeyMemory
 {
 	public static getInstance(): KeyMemory
@@ -22,47 +31,91 @@ export class KeyMemory
 
 	private keysDown: Hash<boolean> = {};
 
-	private keyUpSubscriptions: keyboardEventCallbackTable = {}; 
-	private cleanKeyUpSubscriptions: keyboardEventCallbackTable = {};
+	private subscriptions: Subscriptions =
+	{	keyDown: {}
+	,	keyUp: {}
+	,	keyDownAlone: {}
+	,	keyUpClean: {}
+	};
 
-	private latestKeyDown: string = "";
+	private lastCleanDown: string = "";
 
 	public onKeyDown = (event: keyboardEvent) =>
 	{
+		const wasEmpty = Object.keys(this.keysDown).length === 0;
 		this.keysDown[event.key] = true;
-		this.latestKeyDown = event.key;
+		this.lastCleanDown = null;		
+
+		if(wasEmpty)
+		{	
+			this.lastCleanDown = event.key;
+
+			if(this.attemptConsume(event, this.subscriptions.keyDownAlone))
+				return;
+		}
+
+		this.attemptConsume(event, this.subscriptions.keyDown)
 	}
 
 	public onKeyUp = (event: keyboardEvent) =>
 	{
 		delete this.keysDown[event.key];
-		if(event.key === this.latestKeyDown && this.cleanKeyUpSubscriptions[event.key])
+
+		if(event.key === this.lastCleanDown &&
+			this.attemptConsume(event, this.subscriptions.keyUpClean))
 		{
-			this.notify(this.cleanKeyUpSubscriptions[event.key], event);
-			return event.preventDefault();
+			return;
 		}
 
-		if(this.keyUpSubscriptions[event.key])
-		{
-			this.notify(this.keyUpSubscriptions[event.key], event);
-			return event.preventDefault();
-		}
+		this.attemptConsume(event, this.subscriptions.keyUp);
+	}
+
+	private attemptConsume(event: keyboardEvent, table: keyboardEventCallbackTable)
+	{
+		if(!table[event.key])
+			return false;
+
+		this.notify(table[event.key], event);
+		event.preventDefault();
+		return true;
 	}
 
 	public subscribeOnUp(key: string, callback: keyboardEventCallback)
 	{
-		if(!this.keyUpSubscriptions[key])
-			this.keyUpSubscriptions[key] = new Array();
-
-		this.keyUpSubscriptions[key].push(callback);	
+		this.subscribe(key, callback, this.subscriptions.keyUp);
 	}
 
-	public unsubscribeOnUp(key: string, callback: ()=>void)
+	public unsubscribeOnUp(key: string, callback: keyboardEventCallback)
 	{
-		for(var index = 0; index < this.keyUpSubscriptions[key][index].length ; index++)
-			if(this.keyUpSubscriptions[key][index] === callback)
+		this.unsubscribe(key, callback, this.subscriptions.keyUp);
+	}
+
+	private subscribe(
+		key: string
+	,	callback: keyboardEventCallback
+	,	table: keyboardEventCallbackTable)
+	{
+		if(!table[key])
+			table[key] = new Array();
+
+		table[key].push(callback);
+	}
+
+	private unsubscribe(
+		key: string
+	,	callback: keyboardEventCallback
+	,	table: keyboardEventCallbackTable)
+	{
+		for(let callbackIndex = 0
+		;	callbackIndex < table[key][callbackIndex].length
+		;	callbackIndex++
+		)
+			if(table[key][callbackIndex] === callback)
 			{
-				this.keyUpSubscriptions[key].splice(index, 1);
+				if(table[key].length === 1)
+					delete table[key]
+				else
+					table[key].splice(callbackIndex, 1);
 				return;
 			}
 	}
